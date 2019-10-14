ce606b8c579730369c5e2d147fe4f69c12761bf9
leebonghee <s2cronoss@gmail.com>
2019-10-14 15:01:27 +0900

    Add map

--- app/src/main/java/com/bong/test/Splash.java
+++ app/src/main/java/com/bong/test/Splash.java
@@ -3,7 +3,6 @@ package com.bong.test;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
