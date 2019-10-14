0b4ebdb9dad1af4010d6e1fa5079e1bb0482b06c
leebonghee <s2cronoss@gmail.com>
2019-10-14 14:42:35 +0900

    Add map

--- app/src/main/java/com/bong/test/MainActivity.java
+++ app/src/main/java/com/bong/test/MainActivity.java
@@ -1,16 +1,29 @@
 package com.bong.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name = "loading";
-        String app = "app";
+        Button btn = findViewById(R.id.next_button);
+            btn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/bong/test/SubActivity.java
@@ -0,0 +1,16 @@
+package com.bong.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
